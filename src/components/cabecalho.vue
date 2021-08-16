e8958597460cb42b3c55ee855e7c886f554db13b
Jaummonster <89030731+Jaummonster@users.noreply.github.com>
2021-08-16 17:24:39 -0300

    Add files via upload

---
+++ src/components/cabecalho.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+      <h1> {{valor }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valor : " Forget Exploded by Lin Fnx"
+    }
+  }
+
+}
+</script>
+
+
+<style scoped>
+  header{ 
+    background-color: rgb(255, 0, 212);
+    height: 50px;
+    width: 100%;
+    position: fixed;
+    top: 0px;
+    text-align: center;
+    color: white;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    
+
+}
+
+</style>
